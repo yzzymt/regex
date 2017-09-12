2a355cb39cc2405f1be3743c513febf3448851fe
Guangchi Yuan <mantou605@gmail.com>
2017-09-12 15:51:08 +0800

    Half Ch2

---
+++ Ch2.py
@@ -0,0 +1,37 @@
+# -*- coding: UTF-8 -*-
+import re
+
+rime = open('Data/rime-intro.txt')
+text = rime.read()
+print "Regular Expressions with rime-intro.txt"
+print text
+
+a = re.findall('\d', text)
+print "using \d:", a
+
+a = re.findall('[01]', text)
+print "using [01]:", a
+
+a = re.findall('\D', text)
+print "using \D:", a
+
+a = re.findall('[^0-9]', text)
+print "using [^0-9]:", a
+
+a = re.findall('\W', text)
+print "using \W:", a
+
+a = re.findall('\s', text)
+print "using \s:", a
+#匹配空白符，制表符（\t），换行符（\n），回车符（\r）
+
+a = re.match ('.{8}', text)
+print "using .{8}:", a.group()
+
+a = re.search ('A.{5}T', text)
+print "using A.{5}T:", a.group()
+#使用search寻找整个字符串的第一个匹配
+
+a = re.search ('\w{7}', text)
+print "using \w{7}:", a.group()
+
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # regex
-Introducing Regular Expressions by Michael Fitzgerald using Python
-图灵程序设计丛书：学习正则表达式 人民邮电出版社使用Python实现练习
+### Introducing Regular Expressions by Michael Fitzgerald using Python
+### 图灵程序设计丛书：学习正则表达式 人民邮电出版社 使用Python实现练习
